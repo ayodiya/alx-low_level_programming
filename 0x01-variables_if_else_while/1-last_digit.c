478ca78713793b59cb4521c6383a3f3ef712bbfe
ayodiya <devayodiya@gmail.com>
2021-06-18 05:27:37 -0700

    change lastDigit to lastNum

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -16,19 +16,19 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 
-	lastDigit = n % 10;
+	lastNum = n % 10;
 
-	if (lastDigit > 5)
+	if (lastNum > 5)
 	{
-		printf("Last digit of %d is %d and is greater than 5\n", n, lastDigit);
+		printf("Last digit of %d is %d and is greater than 5\n", n, lastNum);
 	}
-	else if (lastDigit == 0)
+	else if (lastNum == 0)
 	{
-		printf("Last digit of %d is %d and is 0\n", n, lastDigit);
+		printf("Last digit of %d is %d and is 0\n", n, lastNum);
 	}
 	else
 	{
-		printf("Last digit of %d is %d and is less than 6 and not 0", n, lastDigit);
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastNum);
 	}
 
 	return (0);
