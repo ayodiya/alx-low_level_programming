c1646c0e4988aec585386c47a776bbd97dce00c6
ayodiya <devayodiya@gmail.com>
2021-06-22 06:37:23 -0700

    fix betty errors

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * main - print Holberton
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		_putchar(i);
+	}
+
+	return (0);
+}
