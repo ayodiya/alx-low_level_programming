01d53b9ebbdd17c4ab20aaf48b7c0d3691b5accc
ayodiya <devayodiya@gmail.com>
2021-06-24 06:33:29 -0700

    add 2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * mul - return muitiplication of a * b
+ * @a: first number to multiply
+ * @b: secondary number to multiply
+ */
+
+int mul(int a, int b)
+{
+
+	return (a * b);
+
+}
