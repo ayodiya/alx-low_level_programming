a02a399732d4e3d2ef5e1d0b6db77b81cd5d5406
ayodiya <devayodiya@gmail.com>
2021-06-28 10:07:02 -0700

    space out evenly

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -6,11 +6,12 @@
  *
  * Description: swap_int swaps the value of *a and *b
  */
+
 void swap_int(int *a, int *b)
 {
-    int c;
+	int c;
 
-    c = *a;
-    *a = *b;
-    *b = c;
+	c = *a;
+	*a = *b;
+	*b = c;
 }
