bfc7c607dc875507e642eeb4351624dadd0a9118
ayodiya <devayodiya@gmail.com>
2021-07-12 09:20:34 -0700

    add 3-mul.c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - multiply the args
+ * @argc: Argument count
+ * @argv: Array of argument strings
+ *
+ * Return: 0 for successful exit
+ */
+int main(int argc, char *argv[])
+{
+
+	int multi = 0;
+
+	if (argc < 2)
+	{
+		printf("Error");
+	}
+	else
+	{
+
+		multi = atoi(argv[1]) * atoi(argv[2]);
+		printf("%d", multi);
+	}
+
+	return (0);
+}
