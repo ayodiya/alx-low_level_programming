46742f041cce6599988f497b9bb2bab90cc70193
ayodiya <devayodiya@gmail.com>
2021-07-15 18:14:07 -0700

    fix

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -35,7 +35,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	for (i = 0; s1[i]; i++)
 		concat[len++] = s1[i];
 
-	for (index = 0; s2[i] && i < n; i++)
+	for (i = 0; s2[i] && i < n; i++)
 		concat[len++] = s2[i];
 
 	concat[len] = '\0';
