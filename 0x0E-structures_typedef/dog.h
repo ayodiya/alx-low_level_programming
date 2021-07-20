2d65daf5c53f771bd05b90c238a46c677663de02
ayodiya <devayodiya@gmail.com>
2021-07-20 08:21:16 -0700

    correct the code

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -19,11 +19,7 @@ typedef struct dog
 	float age;
 	char *owner;
 }
-	dog_a;
+dog_a;
 
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
 
 #endif
