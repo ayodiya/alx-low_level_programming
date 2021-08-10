ba7acdbe647dd1498bd6f576a9475dde8044f4b3
ayodiya <devayodiya@gmail.com>
2021-08-10 04:41:11 -0700

    fix task

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,45 +1,39 @@
-#include "holberton.h"
+#include "main.h"
+#include <stdlib.h>
 
 /**
- * read_textfile - reads a text file and prints it to the POSIX standard output
- * @filename: pointer to the file name
- * @letters: the number of letters it should read and print
- * Return:  the actual number of letters it could read and print
-*/
-
+ * read_textfile - Reads a text file and prints it to POSIX stdout.
+ * @filename: A pointer to the name of the file.
+ * @letters: The number of letters the
+ *           function should read and print.
+ *
+ * Return: If the function fails or filename is NULL - 0.
+ *         O/w - the actual number of bytes the function can read and print.
+ */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-
-	int file;
-	ssize_t rcount, wcount;
+	ssize_t op, rd, wr;
 	char *buffer;
 
 	if (filename == NULL)
 		return (0);
 
-	file = open(filename, O_RDWR);
-	if (file == -1)
-		return (0);
-
 	buffer = malloc(sizeof(char) * letters);
 	if (buffer == NULL)
+		return (0);
+
+	op = open(filename, O_RDONLY);
+	rd = read(op, buffer, letters);
+	wr = write(STDOUT_FILENO, buffer, rd);
+
+	if (op == -1 || rd == -1 || wr == -1 || wr != rd)
 	{
 		free(buffer);
 		return (0);
 	}
 
-	rcount = read(file, buffer, letters);
-	if (rcount == -1)
-		return (0);
-
-	wcount = write(STDOUT_FILENO, buffer, rcount);
-
-	if (wcount == -1 || rcount != wcount)
-		return (0);
-
 	free(buffer);
+	close(op);
 
-	close(file);
-
-	return (wcount);
+	return (wr);
 }
